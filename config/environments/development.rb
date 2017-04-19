13742d4c4fc927ada1326d3eb532c7a84be07505
Doug Major <demajor@demajorMBP.local>
2017-04-19 12:20:46 -0700

    checkpoint 2 user sign in: completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,29 +54,29 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.secrets.mail_username,
+    password:       Rails.application.secrets.mail_password,
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # config.action_mailer.smtp_settings = {
-  #   user_name: Rails.application.secrets.mail_username,
-  #   password: Rails.application.secrets.mail_password,
-  #   domain: 'gmail.com',
   #   address: 'smtp.gmail.com',
   #   port: '587',
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
-
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: '587',
-  #   domain: 'gmail.com',
+  #   domain: ENV["GMAIL_DOMAIN"],
+  #   # domain: 'gmail.com',
   #   authentication: :plain,
   #   enable_starttls_auto: true,
-  #   user_name: ENV[“GMAIL_USERNAME”],
-  #   password: ENV[“GMAIL_PASSWORD”]
+  #   user_name: ENV["GMAIL_USERNAME"],
+  #   password: ENV["GMAIL_PASSWORD"]
   # }
 
 end
