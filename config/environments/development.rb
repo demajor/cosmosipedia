d7a1ad54d5d823e9aebbd714bfaf50ff7369a842
Doug Major <demajor@demajorMBP.local>
2017-04-26 10:52:31 -0700

    updated gitig and config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,13 +64,13 @@ Rails.application.configure do
 
   # config.action_mailer.delivery_method = :smtp
 
-  # config.action_mailer.smtp_settings = {
-  #   user_name:      Rails.application.secrets.mail_username,
-  #   password:       Rails.application.secrets.mail_password,
-  #   domain:         'gmail.com',
-  #   address:       'smtp.gmail.com',
-  #   port:          '587',
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.smtp_settings = {
+    user_name:      Rails.application.secrets.mail_username,
+    password:       Rails.application.secrets.mail_password,
+    domain:         'gmail.com',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
