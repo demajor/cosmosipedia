2f205e35a9ffa7c3b973347ed14ac02334882c7c
Doug Major <demajor@demajorMBP.local>
2017-04-26 14:48:14 -0700

    cp 5 user roles v4: in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protect_from_forgery with: :exception
-  
-  before_action :authenticate_user!, except: [:index, :show]
 
+  before_action :authenticate_user!, except: [:index, :show]
 
   # def after_sign_in_path_for(resource)
   #   root_path
